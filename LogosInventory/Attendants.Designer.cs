ef25ed9d2ebf0c615d9edad74dbf07ba3ce96862
Rashid-DY <1dryakubu@gmail.com>
2022-08-28 15:45:13 +0000

    Designed attendant table as well as attendant creation form

--- LogosInventory/Attendants.Designer.cs
+++ LogosInventory/Attendants.Designer.cs
@@ -30,15 +30,21 @@ namespace LogosInventory
         private void InitializeComponent()
         {
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Attendants));
             this.AttendantTable = new System.Windows.Forms.DataGridView();
             this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Surname = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column5 = new System.Windows.Forms.DataGridViewImageColumn();
             this.Column6 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NewAttendantBtn = new LogosInventory.CustomButton();
             ((System.ComponentModel.ISupportInitialize)(this.AttendantTable)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NewAttendantBtn)).BeginInit();
             this.SuspendLayout();
             // 
             // AttendantTable
@@ -56,6 +62,7 @@ namespace LogosInventory
             this.AttendantTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column7,
             this.Column1,
+            this.Surname,
             this.Column2,
             this.Column3,
             this.Column4,
@@ -70,10 +77,11 @@ namespace LogosInventory
             // 
             // Column7
             // 
+            this.Column7.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
             this.Column7.HeaderText = "Attendant ID";
             this.Column7.MinimumWidth = 6;
             this.Column7.Name = "Column7";
-            this.Column7.Width = 125;
+            this.Column7.Width = 126;
             // 
             // Column1
             // 
@@ -83,12 +91,19 @@ namespace LogosInventory
             this.Column1.Name = "Column1";
             this.Column1.Width = 109;
             // 
+            // Surname
+            // 
+            this.Surname.HeaderText = "Surname";
+            this.Surname.MinimumWidth = 6;
+            this.Surname.Name = "Surname";
+            this.Surname.Width = 125;
+            // 
             // Column2
             // 
-            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column2.HeaderText = "Full Name";
+            this.Column2.HeaderText = "Other Names";
             this.Column2.MinimumWidth = 6;
             this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
             // 
             // Column3
             // 
@@ -110,6 +125,7 @@ namespace LogosInventory
             // 
             this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
             this.Column5.HeaderText = "Edit";
+            this.Column5.Image = ((System.Drawing.Image)(resources.GetObject("Column5.Image")));
             this.Column5.MinimumWidth = 6;
             this.Column5.Name = "Column5";
             this.Column5.Width = 42;
@@ -118,20 +134,57 @@ namespace LogosInventory
             // 
             this.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
             this.Column6.HeaderText = "Delete";
+            this.Column6.Image = ((System.Drawing.Image)(resources.GetObject("Column6.Image")));
             this.Column6.MinimumWidth = 6;
             this.Column6.Name = "Column6";
             this.Column6.Width = 59;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(307, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(197, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Manage Attendants";
+            // 
+            // NewAttendantBtn
+            // 
+            this.NewAttendantBtn.Image = ((System.Drawing.Image)(resources.GetObject("NewAttendantBtn.Image")));
+            this.NewAttendantBtn.ImageHover = ((System.Drawing.Image)(resources.GetObject("NewAttendantBtn.ImageHover")));
+            this.NewAttendantBtn.ImageNormal = ((System.Drawing.Image)(resources.GetObject("NewAttendantBtn.ImageNormal")));
+            this.NewAttendantBtn.Location = new System.Drawing.Point(350, 412);
+            this.NewAttendantBtn.Name = "NewAttendantBtn";
+            this.NewAttendantBtn.Size = new System.Drawing.Size(81, 62);
+            this.NewAttendantBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.NewAttendantBtn.TabIndex = 3;
+            this.NewAttendantBtn.TabStop = false;
             // 
             // Attendants
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 498);
+            this.Controls.Add(this.NewAttendantBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.AttendantTable);
             this.Name = "Attendants";
             this.Text = "Attendants";
             ((System.ComponentModel.ISupportInitialize)(this.AttendantTable)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NewAttendantBtn)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -140,10 +193,14 @@ namespace LogosInventory
         private System.Windows.Forms.DataGridView AttendantTable;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Surname;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
         private System.Windows.Forms.DataGridViewImageColumn Column5;
         private System.Windows.Forms.DataGridViewImageColumn Column6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private CustomButton NewAttendantBtn;
     }
 }
